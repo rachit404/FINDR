d3ca1d3bf5c2d2f3b23a86eda54ccb388572dcef
spadeking <150600039+rachit404@users.noreply.github.com>
2025-03-29 01:04:58 +0530

    Update recommender.py

--- backend/recommender.py
+++ backend/recommender.py
@@ -9,18 +9,22 @@ features = {
     "Datasets/CoffeeMachineData.csv":{
             "numerical":['Capacity (Liters)','Power Output (kW)','Price (USD)','Brew Time (Minutes)','Weight (kg)'],
             "categorical":['Machine Name','Type','Ease of Use','Brew Quality'],
+            "threshold" : 0.50
         },
     "Datasets/Fruit_Veg_Processing_Machines.csv": {
             "numerical":['Speed (kg/hr)', 'Power Input (kW)', 'Efficiency (%)','Price (USD)'],
             "categorical":['Machine Name', 'MType', 'MachineMaterial','Manufacturer'],
+            "threshold" : 0.50
         },
     "Datasets/grain_machinery_data.csv":{
             "numerical":['Capacity (tons/hour)', 'Power Output (kW)', 'Price (USD)'],
             "categorical":['Grain', 'Machine Name', 'Grain Manufacturer'],
+            "threshold" : 0.50
         },
     "Datasets/Ice_Cream_Makers.csv":{
             "numerical":['Noise Levels','Power(W)','Price (USD)'],
             "categorical":['Machine Name', 'Capacity', 'Batch Output'],
+            "threshold" : 0.50
         },
     "Datasets/juice_makers.csv":
         {
@@ -29,17 +33,10 @@ features = {
                         'Customer Rating', 'Number of Reviews', 'Price (USD)',
                         ],
             "categorical":['Machine Name', 'Material','Type of Juicer'],
+            "threshold" : 0.50
         },
     
 }
-
-# findr_col = {
-#     "Datasets/CoffeeMachineData.csv": '',
-#     "Datasets/Fruit_Veg_Processing_Machines.csv":'',
-#     "Datasets/grain_machinery_data.csv": 'Machine Name',
-#     "/Datasets/Ice_Cream_Makers.csv"
-#     "Datasets/juice_makers.csv": 'Ease of Cleaning',
-# }
 
 datasets = ["Datasets/CoffeeMachineData.csv",
             "Datasets/Fruit_Veg_Processing_Machines.csv",
@@ -54,6 +51,9 @@ def findr(file_path, user_input, findr_col, num_recomm=10):
     df = pd.read_csv(file_path,encoding=result['encoding'])
     categorical_cols = features[file_path]["categorical"]
     numerical_cols = features[file_path]["numerical"]
+    
+    threshold = features[file_path]["threshold"]
+    
     df[categorical_cols] = df[categorical_cols].fillna('')
     df['combined_features'] = df[categorical_cols].apply(lambda x: ' '.join(x).lower(), axis=1)
     for col in numerical_cols:
@@ -81,7 +81,24 @@ def findr(file_path, user_input, findr_col, num_recomm=10):
     idx = matching_rows.index[0]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_indices = [i[0] for i in sim_scores[1:num_recomm+1]]
+    print("Sim_scores: \n")
+    for score in sim_scores:
+        print(score)
+    print("Length: ",len(sim_scores))
+    
+    # sum1 = np.sum(score[1] for score in sim_scores) 
+    
+    sim_score1 = []
+    for score in sim_scores:
+        if score[1] >= threshold:
+            sim_score1.append(score)
+            
+    print("Sim_scores: \n")
+    for score in sim_score1:
+        print(score)
+    print("Length after threshold: ",len(sim_score1))
+    
+    sim_indices = [i[0] for i in sim_score1[1:num_recomm+1]]
 
     df = pd.read_csv(file_path,encoding=result['encoding'])
     data = df.iloc[sim_indices]
